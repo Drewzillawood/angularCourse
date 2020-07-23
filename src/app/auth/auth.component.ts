b6f1d8f8a53a361dc57194cbb6273d5a159948e1
Drew Underwood <drew@uwood.net>
2020-07-23 07:28:35 -0500

    Programmatically Adding a component, doesn't work yet, need to account for data/event binding

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,21 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {AuthResponseData, AuthService} from './auth.service';
 import {Observable} from 'rxjs';
 import {Router} from '@angular/router';
+import {AlertComponent} from '../shared/alert/alert.component';
+import {PlaceholderDirective} from '../shared/placeholder/placeholder.directive';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 export class AuthComponent {
-
   isLoginMode: boolean = true;
   isLoading: boolean = false;
   error: string = null;
+  @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
 
   constructor(private authService: AuthService,
-              private router: Router) {}
+              private router: Router,
+              private componentFactoryResolver: ComponentFactoryResolver) {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -46,13 +49,17 @@ export class AuthComponent {
       },
       errorMessage => {
         this.isLoading = false;
+        this.showErrorAlert(errorMessage);
         this.error = errorMessage;
       });
 
     form.reset();
   }
 
-  onHandleError() {
-    this.error = null;
+  private showErrorAlert(message: string) {
+    const alertComponentFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+    hostViewContainerRef.createComponent(alertComponentFactory);
   }
 }
