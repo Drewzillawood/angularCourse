6027d27e619225b7131aaf5afdbee21523c8e0be
Drew Underwood <drew@uwood.net>
2020-07-27 15:54:50 -0500

    Login Effect

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,16 +1,20 @@
-import {Component, ComponentFactoryResolver, OnDestroy, ViewChild} from '@angular/core';
+import {Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {AuthResponseData, AuthService} from './auth.service';
 import {Observable, Subscription} from 'rxjs';
 import {Router} from '@angular/router';
 import {AlertComponent} from '../shared/alert/alert.component';
 import {PlaceholderDirective} from '../shared/placeholder/placeholder.directive';
+import {Store} from '@ngrx/store';
+
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent implements OnDestroy {
+export class AuthComponent implements OnInit, OnDestroy {
   isLoginMode: boolean = true;
   isLoading: boolean = false;
   error: string = null;
@@ -20,7 +24,18 @@ export class AuthComponent implements OnDestroy {
 
   constructor(private authService: AuthService,
               private router: Router,
-              private componentFactoryResolver: ComponentFactoryResolver) {}
+              private componentFactoryResolver: ComponentFactoryResolver,
+              private store: Store<fromApp.AppState>) {}
+
+  ngOnInit(): void {
+    this.store.select('auth').subscribe(authState => {
+      this.isLoading = authState.loading;
+      this.error = authState.authError;
+      if (this.error) {
+        this.showErrorAlert(this.error);
+      }
+    });
+  }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -38,22 +53,16 @@ export class AuthComponent implements OnDestroy {
     let authObs: Observable<AuthResponseData>;
 
     if (this.isLoginMode) {
-      authObs = this.authService.login(email, password);
+      this.store.dispatch(
+        new AuthActions.LoginStart({ email, password })
+      );
     } else {
       authObs = this.authService.signup(email, password);
     }
 
-    authObs.subscribe(
-      resData => {
-        this.isLoading = false;
-        this.error = null;
-        this.router.navigate(['/recipes']);
-      },
-      errorMessage => {
-        this.isLoading = false;
-        this.showErrorAlert(errorMessage);
-        this.error = errorMessage;
-      });
+    this.store.select('auth').subscribe(authState => {
+
+    });
 
     form.reset();
   }
--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -3,17 +3,27 @@ import * as AuthActions from './auth.actions';
 
 export interface State {
   user: User;
+  authError: string;
+  loading: boolean;
 }
 
 const initialState: State = {
-  user: null
+  user: null,
+  authError: null,
+  loading: false
 };
 
 export function authReducer(
   state: State = initialState,
   action: AuthActions.AuthActions) {
   switch (action.type) {
-    case AuthActions.LOGIN:
+    case AuthActions.LOGIN_START:
+      return {
+        ...state,
+        authError: null,
+        loading: true
+      };
+    case AuthActions.LOGIN_SUCCESS:
       const user = new User(
         action.payload.email,
         action.payload.userId,
@@ -22,7 +32,16 @@ export function authReducer(
       );
       return {
         ...state,
-        user
+        user,
+        authError: null,
+        loading: false
+      };
+    case AuthActions.LOGIN_FAIL:
+      return {
+        ...state,
+        user: null,
+        authError: action.payload,
+        loading: false
       };
     case AuthActions.LOGOUT:
       return {
