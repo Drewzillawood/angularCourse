37790a25f324ae2e8e399618fea51e00bdfca5db
Drew Underwood <drew@uwood.net>
2020-07-31 07:41:03 -0500

    Adjusting our angular app to accommodate Angular Universal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {Store} from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,12 @@ import * as AuthActions from './auth/store/auth.actions';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private store: Store<fromApp.AppState>) {}
+  constructor(private store: Store<fromApp.AppState>,
+              @Inject(PLATFORM_ID) private platformId) {}
 
   ngOnInit(): void {
-    this.store.dispatch(new AuthActions.AutoLoginStart());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLoginStart());
+    }
   }
 }
